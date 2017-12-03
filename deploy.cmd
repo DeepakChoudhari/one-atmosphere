1596e30ed9f4b6a96c481eeceb6faa3df6fa2dce
Deepak Choudhari <Deepak.Choudhari@outlook.com>
2017-12-03 08:46:21 -0600

    Updates to deployment scripts

--- deploy.cmd
+++ deploy.cmd
@@ -101,7 +101,7 @@ call :SelectNodeVersion
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install
-  call :ExecuteCmd %DEPLOYMENT_TARGET%\node_modules\bin\ng build
+  call :ExecuteCmd %DEPLOYMENT_TARGET%\node_modules\.bin\ng build
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
